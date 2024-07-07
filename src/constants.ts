fa04a6cd5b9ae87005b56b411967105b4b362e2b
MiracleUFO <miracle.ufodiama@raisin.com>
2024-07-07 17:39:05 +0200

    fix: rate limiting

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,10 @@
 const TOO_MANY_REQUESTS = 'Too Many Requests';
 
 const EXPRESS_RATE_LIMITER_OPTS = {
-  windowMs: 1000,
+  windowMs: 1 * 60 * 1000,
   limit: 10,
   standardHeaders: true,
-  skipFailedRequests: true,
+  skipFailedRequests: false,
 };
 
 export {
